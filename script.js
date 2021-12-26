c9eaf1bf83d4c37e4c82a9a3ccf0829885267fe5
dronkillerShark <88827209+dronkillerShark@users.noreply.github.com>
2021-12-26 12:02:14 +0200

    added some features

--- script.js
+++ script.js
@@ -6,11 +6,34 @@ const changeSize = document.querySelector(".change");
 const sizeInput = document.querySelector(".changeSize");
 const err = document.querySelector(".error");
 const eraser = document.querySelector(".erase");
+const redInp = document.querySelector(".red");
+const greenInp = document.querySelector(".green");
+const blueInp = document.querySelector(".blue");
+const changeColorButton = document.querySelector(".change__color");
+const colorError = document.querySelector(".err");
 
 let isDown = false;
 let color = `rgb(0, 0, 0)`;
 let size = 5;
 let isErase = false;
+
+changeColorButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    colorError.textContent = "";
+    colorError.style.color = "red";
+    if(redInp.value == "") redInp.value = 0;
+    if(blueInp.value == "") blueInp.value = 0;
+    if(greenInp.value == "") greenInp.value = 0;
+    if(!greenInp.value 
+        || 
+        !blueInp.value 
+        ||
+        !redInp.value){
+            colorError.textContent = "please type a number!";
+        }
+    color = `rgb(${redInp.value}, ${greenInp.value}, ${blueInp.value})`;
+    redInp.value = blueInp.value = greenInp.value = '';
+})
 
 const addEventToErase = function(){
     eraser.addEventListener("click", ()=>{
@@ -44,7 +67,7 @@ const createNewPixel = function(e){
 }
 
 const removePixel = function(e){
-    ctx.clearRect((e.clientX - 630), (e.clientY - 215), size, size)
+    ctx.clearRect((e.clientX - 630), (e.clientY - 215), size, size);
 }
 
 drawingCanvas.addEventListener("pointermove", (e) =>{
