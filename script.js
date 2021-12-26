b1f7e033a756019a5283606ee3e76fedb6d9d72b
dronkillerShark <88827209+dronkillerShark@users.noreply.github.com>
2021-12-26 09:54:47 +0200

    added eraser

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ const ctx = canvas.getContext('2d');
 const changeSize = document.querySelector(".change");
 const sizeInput = document.querySelector(".changeSize");
 const err = document.querySelector(".error");
+const eraser = document.querySelector(".erase");
 
 let isDown = false;
 let color = `rgb(0, 0, 0)`;
 let size = 5;
+let isErase = false;
+
+const addEventToErase = function(){
+    eraser.addEventListener("click", ()=>{
+    isErase = true;
+    eraser.textContent = "draw";
+    eraser.addEventListener("click", ()=>{
+    isErase = false;
+    eraser.textContent = "erase";
+    addEventToErase();
+    })
+})
+}
+
+addEventToErase();
 
 changeSize.addEventListener("click", (e)=>{
     e.preventDefault();
@@ -23,8 +39,12 @@ changeSize.addEventListener("click", (e)=>{
 
 const createNewPixel = function(e){
     ctx.fillStyle = color;
-    ctx.fillRect((e.clientX - 560), (e.clientY - 215), size, size);
+    ctx.fillRect((e.clientX - 630), (e.clientY - 215), size, size);
     ctx.stroke();
+}
+
+const removePixel = function(e){
+    ctx.clearRect((e.clientX - 630), (e.clientY - 215), size, size)
 }
 
 drawingCanvas.addEventListener("pointermove", (e) =>{
@@ -34,8 +54,10 @@ drawingCanvas.addEventListener("pointermove", (e) =>{
     drawingCanvas.addEventListener("pointerup", (e) =>{
         isDown = false;
     })
-    if(isDown){
+    if(isDown && !isErase){
         createNewPixel(e);
+    } else if (isDown && isErase){
+        removePixel(e);
     }
 })
 
